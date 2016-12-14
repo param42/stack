a817289f15d39e51cfa4daa6da004fd976a6d74c
param42 <param42@ya.ru>
2016-12-15 00:28:10 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,61 +1,63 @@
 #ifndef STACK_STACK_HPP
 #define STACK_STACK_HPP
 
-#include <iostream>
+#include "allocator.hpp"
+#include <stdexcept>
 
 template <typename T>
-class stack {
+class stack : private allocator<T>
+{
 public:
+    stack(size_t size = 0);
 
-    stack();
-    ~stack();
-
-    auto count() const noexcept -> size_t ;
-    auto push(T const& value) -> void;
-    auto pop() -> T;
-
-
-private:
-    T * array_;
-    size_t array_size_;
-    size_t count_;
+    auto count() const noexcept -> size_t;
+    auto push(const T & element) noexcept -> void;
+    auto pop() noexcept -> void;
+    auto top() const /* strong */ -> T;
+    auto empty() const noexcept -> bool;
 };
 
+template <typename T>
+stack<T>::stack(size_t size) : allocator<T>(size) {}
 
 template <typename T>
-stack<T>::stack() : count_(0), array_(nullptr),  array_size_(0) {
+auto stack<T>::count() const noexcept -> size_t
+{
+    return allocator<T>::_count;
 }
 
 template <typename T>
-stack<T>::~stack() {
-    delete[] array_;
-}
-
-template <typename T>
-auto stack<T>::count() const noexcept -> size_t {
-    return count_;
-}
-
-template <typename T>
-auto stack<T>::push(T const& value) -> void {
-    if (count_ == array_size_) {
-        size_t size = array_size_ * 2 + (array_size_ == 0);
-        T * m_array = new T[size];
-        std::copy(array_, array_ + array_size_, m_array);
-        delete[] array_;
-        array_ = m_array;
-        array_size_ = size;
+auto stack<T>::push(const T & element) noexcept -> void
+{
+    try
+    {
+        allocator<T>::allocate();
+        allocator<T>::_ptr[allocator<T>::_count] = element;
+        allocator<T>::_count++;
     }
-    ++count_;
-    array_[count_ - 1] = value;
+    catch (...) {}
 }
 
 template <typename T>
-auto stack<T>::pop() -> T{
-    if(count_==0)
-        throw std::logic_error("stack is empty!");
+auto stack<T>::pop() noexcept -> void
+{
+    if (allocator<T>::_count)
+        allocator<T>::_count--;
+}
 
-    return array_[--count_];
+template <typename T>
+auto stack<T>::top() const /* strong */ -> T
+{
+    if (allocator<T>::_count)
+        return allocator<T>::_ptr[allocator<T>::_count - 1];
+    else
+        throw std::underflow_error("Stack is already empty.");
+}
+
+template <typename T>
+auto stack<T>::empty() const noexcept -> bool
+{
+    return !allocator<T>::_count;
 }
 
 #endif
